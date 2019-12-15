7113936e41bdbbf56b3e5c84f1a97210bdb9359c
Nadjem Nour El Imane <47980833+NINadjem@users.noreply.github.com>
2019-12-15 23:30:29 +0100

    *

--- SpqApp/src/spqapp/SPQ.java
+++ SpqApp/src/spqapp/SPQ.java
@@ -2,6 +2,8 @@ package spqapp;
 
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.Iterator;
+import java.util.stream.Stream;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -14,35 +16,62 @@ import java.util.Comparator;
  */
 public class SPQ {
 
-    private double current_time; // temp actuel== notre horloge
+    private static double current_time; // temp actuel== notre horloge
     private final double k;
     private ArrayList<InputElement> input; // le tableau 
     private ArrayList<ResultElement> result = new ArrayList<>(); // ce qu'on va afficher
     private ArrayList<Character> waiting_list = new ArrayList<>(); // la liste d'attente qui contiens les element qu'on a sauter ordonnée par temp d'arrivé
-    private int inputListCurrentIndex; // un pointeur qui indique la colone courante
+    private int inputListCurrentIndex; // un p ointeur qui indique la colone courante
     private boolean done = false; // indicateur d'etat d'avancement du travail
-    private static Comparator comparator=new Comparator<Character>() { // trier en ordre alphabetique
-            @Override
-            public int compare(Character o1, Character o2) {
+    public static Character lastChar = ' ';
+    private static Comparator simpleComparator = new Comparator<Character>() { // trier en ordre alphabetique
+        @Override
+        public int compare(Character o1, Character o2) {
+            return Character.compare(o1, o2);
+        }
+    };
+    public static Comparator comparator = new Comparator<Character>() { // trier en ordre alphabetique
+        @Override
+        public int compare(Character o1, Character o2) {
+            System.out.println(o1+" **********"+o2+ " *********"+lastChar +"****"+current_time);
+            int index = InputElement.alphabets.indexOf(lastChar);
+            if (index == -1) {
+                System.out.println(".compare()==-1");
                 return Character.compare(o1, o2);
+            } else {
+                System.out.println(".compare()!=-1");
+                int n=(Character.compare(lastChar, o1));
+                int number=n*(Character.compare(lastChar,o2));
+                if(number<0){
+                    System.out.println(".compare() diffrent");
+                    return (-1*Character.compare(o1, o2));
+                }else{
+                    if(number==0){
+                        if(n==0) return -1;
+                        else 
+                        return 1;
+                    }else{
+                    System.out.println(".compare()");
+                    return Character.compare(o1, o2);}
+                }
             }
-        };
-    
-    
+        }
+    };
+
     //constructeur avec le tableau seulement
     public SPQ(ArrayList<InputElement> input) {
         this.input = input;
         k = 0.5; // fixé par defaut 0.5 
         execute();// pour commancer le travail
     }
-    
+
     //constructeur avec tableur et k
-    public SPQ(ArrayList<InputElement> input,double k) {
+    public SPQ(ArrayList<InputElement> input, double k) {
         this.input = input;
-        this.k=k;
+        this.k = k;
         execute();// pour commancer le travail
     }
-    
+
     // cette methode est la recpencable d'ordonnacement
     private void execute() {
         if (!input.isEmpty()) { // on test d'abord que le tableau contiens des elements
@@ -53,14 +82,15 @@ public class SPQ {
             }
         }
     }
-    
+
     // cette methode va passer le 1 er element dans une colonne indiqué par son index qui est "elementIndexInInput" 
     private void getPassTheFirstWaitingElement(int elementIndexInInput) {
         InputElement inputElement = input.get(elementIndexInInput); // sauvgarder une reference de la colonne
         Character token_element = inputElement.getTheNextElement();// sauvgarder une reference de l'element qu'on va passer
+        lastChar=token_element;
         current_time += k; // incrémenter le temps
         result.add(new ResultElement(current_time, token_element)); // ajuter au resultat le fait qu'on a passer l'element a le temps courant
-        inputElement.removeElementFromQueue(); // suprimer l'element de la liste (liste d'attente de la colonne)
+        inputElement.removeElementFromQueue(token_element); // suprimer l'element de la liste (liste d'attente de la colonne)
         int nextColumn = getTheNextColumnIndex(); // prédire la prochaine colonne a executer
         System.out.println(inputElement.waiting_list); // khorti pour debugger
         if (nextColumn != elementIndexInInput || nextColumn == input.size() - 1) {
@@ -100,31 +130,34 @@ public class SPQ {
     //cette methode va passer le 1 er element dans la liste d'attente
     private void getPassNextInWaitingList() {
         if (!waiting_list.isEmpty()) { // pas vide
+            waiting_list.sort(comparator);
             current_time += k; // incrementer
             System.out.println("took " + waiting_list.get(0) + " at " + current_time + " getPassNextInWaitingList()");//debug
             result.add(new ResultElement(current_time, waiting_list.get(0))); // ajouter au resultat
+            lastChar=waiting_list.get(0);
             waiting_list.remove(0);// supprimer car on a passer cet element
         }
     }
-    
+
     // cette methode va passer tous les elements de la liste d'attente par ordre alphabetique
     private void getPassAllInWaitingList() {
-        System.out.println("spqapp.SPQ.getPassAllInWaitingList()");//debug
+        System.out.println("spqapp.SPQ.getPassAllInWaitingList()"+current_time+"***"+lastChar);//debug
         waiting_list.sort(comparator);
         for (Character character : waiting_list) { // parcourir
             current_time += k; // incrementer
             System.out.println("took " + character + " " + current_time);// debug
             result.add(new ResultElement(current_time, character));// ajouter au resultat
+            lastChar=character;
         }//fin boucle
         waiting_list = new ArrayList<>();// effacer tt
         input = new ArrayList<>(); // efacer le tableau
     }
-    
+
     //cette methode nous donne le resultats obtenu en tableau
     public ArrayList<ResultElement> getTheResultSet() {
         return result;
     }
-    
+
     // cette methode ecrit le resultat
     void printTheResult() {
         for (ResultElement re : result) {
@@ -152,10 +185,11 @@ public class SPQ {
         }
 
     }
-    
+
     // la struture d'un element du donnée (tableau)
     public static class InputElement {
 
+        public static ArrayList<Character> alphabets = new ArrayList<>();
         private double time; // temps
         private ArrayList<Character> waiting_list; // les elements qui ont entré
 
@@ -163,6 +197,15 @@ public class SPQ {
             this.time = time;
             this.waiting_list = waiting_list;
             this.waiting_list.sort(comparator);
+            for (Character c : this.waiting_list) {
+                if (!alphabets.contains(c)) {
+                    alphabets.add(c);
+                    System.err.println(c);
+                }
+            }
+            alphabets.sort(simpleComparator);
+            
+            
 
         }
 
@@ -173,16 +216,17 @@ public class SPQ {
         public ArrayList<Character> getWaiting_list() {
             return waiting_list;
         }
-        
+
         //pour supp 1 er element de la liste d attente
-        public void removeElementFromQueue() {
-            if (!waiting_list.isEmpty()) {
-                waiting_list.remove(0);
+        public void removeElementFromQueue(Character c) {
+            if (!waiting_list.isEmpty()&&waiting_list.contains(c)) {
+                waiting_list.remove(c);
             }
         }
-        
+
         // pour savoir quel est l caractere  a wela b wela ....
         public Character getTheNextElement() {
+            
             if (!waiting_list.isEmpty()) {
                 return waiting_list.get(0);
             } else {
@@ -192,5 +236,4 @@ public class SPQ {
 
     }
 
-    
 }
--- SpqApp/src/spqapp/SpqApp.java
+++ SpqApp/src/spqapp/SpqApp.java
@@ -15,7 +15,7 @@ public class SpqApp {
 
 
     public static void main(String[] args) {
-       ArrayList<SPQ.InputElement> inputList=new ArrayList();
+       /*ArrayList<SPQ.InputElement> inputList=new ArrayList();
        ArrayList<Character> wList=new ArrayList<>();
        wList.add('a');
        wList.add('c');
@@ -51,9 +51,111 @@ public class SpqApp {
        wList.add('c');
        inputList.add(new SPQ.InputElement(5.5, wList));
        
-       new SPQ(inputList).printTheResult();
+       new SPQ(inputList).printTheResult();*/
        
+       // 2eme exemple 
+        /*ArrayList<SPQ.InputElement> inputList = new ArrayList();
+        ArrayList<Character> wList = new ArrayList<>();
+        wList.add('a');
+        wList.add('b');
+        wList.add('c');
+        inputList.add(new SPQ.InputElement(1, wList));
+
+        wList = new ArrayList<>();
+        wList.add('b');
+        wList.add('c');
+        inputList.add(new SPQ.InputElement(2, wList));
+
+        wList = new ArrayList<>();
+        wList.add('a');
+        wList.add('b');
+        inputList.add(new SPQ.InputElement(3, wList));
+
+        wList = new ArrayList<>();
+        wList.add('a');
+        wList.add('c');
+        inputList.add(new SPQ.InputElement(4, wList));
+
+        wList = new ArrayList<>();
+        wList.add('b');
+        inputList.add(new SPQ.InputElement(4.5, wList));
+
+        wList = new ArrayList<>();
+        wList.add('a');
+        wList.add('c');
+        inputList.add(new SPQ.InputElement(5, wList));
+
+        wList = new ArrayList<>();
+        wList.add('b');
+        wList.add('c');
+        inputList.add(new SPQ.InputElement(5.5, wList));
+
+        wList = new ArrayList<>();
+        wList.add('a');
+        wList.add('b');
+        wList.add('c');
+        inputList.add(new SPQ.InputElement(6, wList));
+       new SPQ(inputList).printTheResult();*/
+       
+       // 3eme exemple
+       /* ArrayList<SPQ.InputElement> inputList = new ArrayList();
+        ArrayList<Character> wList = new ArrayList<>();
+        wList.add('a');
+        wList.add('b');
+        wList.add('c');
+        inputList.add(new SPQ.InputElement(1, wList));
+
+        wList = new ArrayList<>();
+        wList.add('b');
+        wList.add('c');
+        inputList.add(new SPQ.InputElement(2, wList));
+
+        wList = new ArrayList<>();
+        wList.add('a');
+        wList.add('c');
+        inputList.add(new SPQ.InputElement(2.5, wList));
+
+        wList = new ArrayList<>();
+        //wList.add('a');
+        wList.add('c');
+        inputList.add(new SPQ.InputElement(3, wList));
+
+        wList = new ArrayList<>();
+        wList.add('b');
+        wList.add('c');
+        inputList.add(new SPQ.InputElement(3.5, wList));
+
+        wList = new ArrayList<>();
+        wList.add('a');
+        wList.add('b');
+        inputList.add(new SPQ.InputElement(4.5, wList));
+
+        wList = new ArrayList<>();
+        wList.add('a');
+        wList.add('c');
+        inputList.add(new SPQ.InputElement(5, wList));
+
+        wList = new ArrayList<>();
+        //wList.add('a');
+        wList.add('b');
+        wList.add('c');
+        inputList.add(new SPQ.InputElement(6, wList));
         
+        wList = new ArrayList<>();
+        wList.add('a');
+        wList.add('b');
+        inputList.add(new SPQ.InputElement(6.5, wList));
+       
+        wList = new ArrayList<>();
+        //wList.add('a');
+        wList.add('c');
+        inputList.add(new SPQ.InputElement(7, wList));
+        new SPQ(inputList).printTheResult();*/
+        /*SPQ.InputElement.alphabets.add('a');SPQ.InputElement.alphabets.add('b');SPQ.InputElement.alphabets.add('c');
+        ArrayList list=new ArrayList<Character>();list.add('a');list.add('b');list.add('c');list.add('b');SPQ.lastChar='b';list.sort(SPQ.comparator);System.out.println(list);*/
+   
+       
     }
+    
     
 }
